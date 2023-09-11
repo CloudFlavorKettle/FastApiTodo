9613c6ea86d097112be9f465694c62f05c446bd8
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-09-11 13:32:57 +0900

    orm join

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -3,13 +3,14 @@ from fastapi.testclient import TestClient
 from database.orm import ToDo
 from main import app
 
+
 def test_health_check(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}
 
 
-def test_get_todos(client ,mocker):
+def test_get_todos(client, mocker):
     # order=ASC
     mocker.patch("main.get_todos", return_value=[
         ToDo(id=1, contents="FastAPI Section 0", is_done=True),
@@ -34,3 +35,44 @@ def test_get_todos(client ,mocker):
 
         ]
     }
+
+
+def test_get_todo(client, mocker):
+    # 200
+    mocker.patch(
+        "main.get_todo_by_todo_id",
+        return_value=ToDo(id=1, contents="todo", is_done=True),
+    )
+
+    response = client.get("/todos/1")
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "contents": "todo", "is_done": True}
+
+    # 404
+    mocker.patch("main.get_todo_by_todo_id", return_value=None)
+
+    response = client.get("/todos/1")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "ToDo Not Found"}
+
+
+def test_create_todo(client, mocker):
+    create_spy = mocker.spy(ToDo, "create")
+    mocker.patch(
+        "main.create_todo",
+        return_value=ToDo(id=1, contents="todo", is_done=True),
+    )
+
+    body = {
+        "contents": "test",
+        "is_done": False,
+    }
+    response = client.post("/todos", json=body)
+
+    assert create_spy.spy_return.id is None
+    assert create_spy.spy_return.contents == "test"
+    assert create_spy.spy_return.is_done is False
+
+    assert response.status_code == 201
+    assert response.json() == {"id": 1, "contents": "todo", "is_done": True}
+
