1c957037e6ea7c38c70092aea41d2c1c156910fd
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-09-18 10:10:22 +0900

    로그인 API 구현

--- src/api/user.py
+++ src/api/user.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
+from database.orm import User
 from database.repository import UserRepository
-from schema.request import SignUpRequest
-from schema.response import UserSchema
+from schema.request import SignUpRequest, LogInRequest
+from schema.response import UserSchema, JWTResponse
 from service.user import UserService
 
 router = APIRouter(prefix="/users")
@@ -26,5 +27,24 @@ def user_sign_up_handler(
 
 
 @router.post("/log-in")
-def user_log_in_handler():
-    return True
+def user_log_in_handler(
+        request: LogInRequest,
+        user_service: UserService = Depends(),
+        user_repo: UserRepository = Depends(),
+):
+    user: User | None = user_repo.get_user_by_username(username=request.username)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User Not Found")
+
+    verified: bool = user_service.verify_password(
+        plain_password=request.password,
+        hashed_password=user.password,
+    )
+
+    if not verified:
+        raise HTTPException(status_code=401, detail="Not Authorized")
+
+    access_token: str = user_service.create_jwt(username=user.username)
+
+    return JWTResponse(access_token=access_token)
--- src/service/user.py
+++ src/service/user.py
@@ -1,8 +1,13 @@
+from datetime import datetime, timedelta
+
 import bcrypt
+from jose import jwt
 
 
 class UserService:
     encoding: str = "UTF-8"
+    secret_key: str = "17339535eacf4e61ba24686c9ccb802b79f62d50f1bf46bc8fd1855293fe496f"
+    jwt_algorithm = "HS256"
 
     def hash_password(self, plain_password: str) -> str:
         hashed_password: bytes = bcrypt.hashpw(
@@ -10,3 +15,21 @@ class UserService:
             salt=bcrypt.gensalt(),
         )
         return hashed_password.decode(self.encoding)
+
+    def verify_password(
+            self, plain_password: str, hashed_password: str
+    ) -> bool:
+        return bcrypt.checkpw(
+            plain_password.encode(self.encoding),
+            hashed_password.encode(self.encoding)
+        )
+
+    def create_jwt(self, username: str) -> str:
+        return jwt.encode(
+            {
+                "sub": username,
+                "exp": datetime.now() + timedelta(days=1),
+            },
+            self.secret_key,
+            algorithm=self.jwt_algorithm,
+            )
