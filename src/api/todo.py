148bcd99b446521ee0cce04ab647ba8b99eaec97
CloudFlavorKettle <ekaqh8624@gmail.com>
2023-09-18 10:59:51 +0900

    캐싱 및 redis 연결

--- src/api/todo.py
+++ src/api/todo.py
@@ -4,20 +4,32 @@ from fastapi import Depends, HTTPException, Body, APIRouter
 from sqlalchemy.orm import Session
 
 from database.connection import get_db
-from database.orm import ToDo
-from database.repository import get_todos, get_todo_by_todo_id, create_todo, update_todo, delete_todo
+from database.orm import ToDo, User
+from database.repository import get_todos, get_todo_by_todo_id, create_todo, update_todo, delete_todo, UserRepository
 from schema.request import CreateToDoRequest
 from schema.response import ToDoListSchema, ToDoSchema
+from security import get_access_token
+from service.user import UserService
 
 router = APIRouter()
 
 
 @router.get("/todos")
 def get_todos_handler(
+        access_token: str = Depends(get_access_token),
         order: str | None = None,
+        user_service: UserService = Depends(),
+        user_repo: UserRepository = Depends(),
         session: Session = Depends(get_db),
-):
-    todos: List[ToDo] = get_todos(session=session)
+) -> ToDoListSchema:
+
+    username: str = user_service.decode_jwt(access_token=access_token)
+
+    user: User | None = user_repo.get_user_by_username(username=username)
+    if not user:
+        raise HTTPException(status_code=404, detail="User Not Found")
+
+    todos: List[ToDo] = user.todos
     if order and order == "DESC":
         return ToDoListSchema(
             todos=[ToDoSchema.from_orm(todo) for todo in todos[::-1]]
